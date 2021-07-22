008e6f66ab30a4f05dda6c6896c2d49c80e95630
Gitu Khanchi <gitukhanchi99@gmail.com>
2021-07-23 00:33:55 +0530

    Added backend and frontend code

---
+++ backend/todos/views.py
@@ -0,0 +1,20 @@
+from rest_framework import generics
+from .models import Todo
+from .serializers import TodoSerializer
+
+
+class ListTodo(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class DetailTodo(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer   
+
+
+
+    
+
+
+# Create your views here.
